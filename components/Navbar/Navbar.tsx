fe88d65324626d9d5d53d289541f3cc8f8396fdc
Faizan Ali <fa1694701@gmail.com>
2023-04-26 21:32:04 -0700

    new changing

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div style={{backgroundColor:"red"}}>
+
+<h1 className="text-4xl text-center p-4 text-white fonting">Simple Comment Board</h1>
+
+        </div>
+    )
+}
+export default Navbar;
