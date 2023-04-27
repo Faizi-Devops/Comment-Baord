f1ae25f2aaceb52ebba12311161131c3c7de4637
Faizan Ali <fa1694701@gmail.com>
2023-04-26 23:12:34 -0700

    new changing

--- components/Comment/Coment.tsx
+++ components/Comment/Coment.tsx
@@ -3,10 +3,10 @@ const Coment = () => {
         <div className="pt-6">
             <div className=" mx-auto max-w-4xl carding p-6 bg-white border border-gray-200 rounded-sm shadow dark:bg-gray-800 dark:border-gray-700">
                 <p className="mb-3 text-3xl fonting font-normal text-gray-700 dark:text-gray-400">Add your comment</p>
-                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Your Name"></input>
+                <textarea className="block w-full h-20 px-4 py-2 mt-3 text-gray-700 border border-gray-300 rounded-md resize-none focus:outline-none    sm:text-sm" placeholder="Your Comment">
+</textarea>
+
             </div>
 
 
